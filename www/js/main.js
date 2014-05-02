d44a9790f8676f0a38bda96a9d634f2495ded80f
Your Name <you@domain.com>
2014-05-02 14:11:23 +0200

    Initialize FW7

--- www/js/main.js
+++ www/js/main.js
@@ -1,17 +1,11 @@
-// Initialize your app
-var myApp = new Framework7();
-
-// Export selectors engine
-var $$ = Framework7.$;
-
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = app.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
 // Event listener to run specific code for specific pages
-$$(document).on('pageInit', function (e) {
+$(document).on('pageInit', function (e) {
     var page = e.detail.page;
     // If it is About page
     if (page.name === 'about') {
@@ -25,7 +19,7 @@ $$(document).on('pageInit', function (e) {
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
-	mainView.loadContent(
+  mainView.loadContent(
         '<!-- Top Navbar-->' +
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
@@ -48,5 +42,5 @@ function createContentPage() {
         '  </div>' +
         '</div>'
     );
-	return;
+  return;
 }
